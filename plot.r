4434e192e24deb22880f198b00f984ed08a74124
Your Name <you@example.com>
2018-01-25 15:44:21 +0200

    Local chanes

--- plot.r
+++ plot.r
@@ -1,11 +1,10 @@
 library(gtools)
+library(optparse)
 
 GAMMAS = list()
 GAMMAS$HHH = c(5, 9, 13)^(-1)
 GAMMAS$HHL = c(9, 13, -1)^(-1)
-GAMMAS$LLL = c(-1, -0.5, 0)^(-1) # TODO: Make sure that the extreme value index for Beta distribution is correct.
-
-# TODO: Make sure that everything actually works.
+GAMMAS$LLL = c(-1, -0.5^(-1), 0) # TODO: Make sure that the extreme value index for Beta distribution is correct.
 
 help="
     
@@ -61,10 +60,12 @@ process_file = function(str){
     raw_data  =   optimize_array(tmp, gammas)
     dffs      =   t(apply(raw_data, 1, function(x) x-gammas))
 
-    out = list()
-    out[[ paste( sub("data/out_", "", str ), as.character(1/gammas[1]), sep="_") ]] = dffs[,1]
-    out[[ paste( sub("data/out_", "", str ), as.character(1/gammas[2]), sep="_") ]] = dffs[,2]
-    out[[ paste( sub("data/out_", "", str ), as.character(1/gammas[3]), sep="_") ]] = dffs[,3]
+    out     = list()
+    gammas  = apply(matrix(gammas), 1, function(x) if(x==0){return(x);}else{return(1/x);}); 
+
+    out[[ paste( sub("data/out_", "", str ), as.character(gammas[1]), sep="_") ]] = dffs[,1]
+    out[[ paste( sub("data/out_", "", str ), as.character(gammas[2]), sep="_") ]] = dffs[,2]
+    out[[ paste( sub("data/out_", "", str ), as.character(gammas[3]), sep="_") ]] = dffs[,3]
 
     return(out)
 
@@ -87,5 +88,37 @@ process_files = function(fnames){
         out = c(out, process_file(f))
     }
     return(out)
+}
+
+# Parse arguments
+option_list = list(
+  make_option("--files", type="character", default=NA,
+                help="Which files to process in form [fname1],[fname2],....,[fnameN]"),
+  make_option("--op", type="character", default=NA,
+                help="What to do with the data. Choices: histogram, barplot")
+);
+
+opt_parser  =   OptionParser(option_list=option_list);
+OPT         =   parse_args(opt_parser);
+
+if(NA %in% OPT){ 
+    stop("Missing arguments. See --help.");
+}
+
+files 	    =   strsplit(OPT$files, split=",")[[1]]
+data 	    =   process_files(files)
+
+if(OPT$op == "histogram"){
+    
+    # The number of files has to be exactly 2.
+    if(length(files)!=2){stop("The histogram plotter requires exactly 2 files.");}
+    
+    # Six histograms in one plot.
+    par(mfrow=c(2,3))
+
+    for(f in names(data)){
+        hist(get(f, data), main=f)
+    }
+
 }
 
